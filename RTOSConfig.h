11a0b5d56b1dc33c9ff7d6f29f15d44f303a9972
gototop <asfdsdt123ygq@gmail.com>
2014-03-20 13:37:44 +0800

    insert gdb

--- RTOSConfig.h
+++ RTOSConfig.h
@@ -6,10 +6,10 @@
 #define vPortSVCHandler SVC_Handler
 
 #define configUSE_PREEMPTION		1
-#define configUSE_IDLE_HOOK		1
-#define configUSE_TICK_HOOK		1
-#define configCPU_CLOCK_HZ		( ( unsigned long ) 72000000 )	
-#define configTICK_RATE_HZ		( ( portTickType ) 100 )
+#define configUSE_IDLE_HOOK			1
+#define configUSE_TICK_HOOK			1
+#define configCPU_CLOCK_HZ			( ( unsigned long ) 72000000 )	
+#define configTICK_RATE_HZ			( ( portTickType ) 100 )
 #define configMAX_PRIORITIES		( ( unsigned portBASE_TYPE ) 5 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 128 )
 #define configTOTAL_HEAP_SIZE		( ( size_t ) ( 17 * 1024 ) )
@@ -17,7 +17,7 @@
 #define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		1
-#define configUSE_MUTEXES		1
+#define configUSE_MUTEXES			1
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 		0
@@ -26,13 +26,13 @@
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
-#define INCLUDE_vTaskPrioritySet	1
-#define INCLUDE_uxTaskPriorityGet	1
-#define INCLUDE_vTaskDelete		1
+#define INCLUDE_vTaskPrioritySet		1
+#define INCLUDE_uxTaskPriorityGet		1
+#define INCLUDE_vTaskDelete				1
 #define INCLUDE_vTaskCleanUpResources	0
-#define INCLUDE_vTaskSuspend		1
-#define INCLUDE_vTaskDelayUntil		1
-#define INCLUDE_vTaskDelay		1
+#define INCLUDE_vTaskSuspend			1
+#define INCLUDE_vTaskDelayUntil			1
+#define INCLUDE_vTaskDelay				1
 
 /* Tracer functions  */
 #define traceTASK_SWITCHED_OUT			my_switched_out_task
